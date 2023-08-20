03e3e898a7965408f2f00d4b9d11872591652e1e
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-08-20 18:08:35 +0400

    Add random for gifs for each new game

--- src/GetCards.jsx
+++ src/GetCards.jsx
@@ -1,7 +1,11 @@
 export default async function getGifs(n) {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const offset = getRandomInt(100);
   try {
     const response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=i7wu21HcofsCVQkecmNw6G9Io6QCi6R9&q=cats&limit=${n}&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
+      `https://api.giphy.com/v1/gifs/search?api_key=i7wu21HcofsCVQkecmNw6G9Io6QCi6R9&q=cats&limit=${n}&offset=${offset}&rating=g&lang=en&bundle=messaging_non_clips`,
       { mode: 'cors' }
     );
     const data = await response.json();
